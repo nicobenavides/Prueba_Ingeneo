47bfa721d630f83c6e4fc7f2d08d2a69d362edf0
nicobenavides <64330919+nicobenavides@users.noreply.github.com>
2021-07-19 20:24:34 -0500

    'backend'

---
+++ nicocine/src/main/java/co/com/ingeneo/nicocine/NicocineApplication.java
@@ -0,0 +1,13 @@
+package co.com.ingeneo.nicocine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicocineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicocineApplication.class, args);
+	}
+
+}
---
+++ nicocine/src/test/java/co/com/ingeneo/nicocine/NicocineApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ingeneo.nicocine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicocineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
