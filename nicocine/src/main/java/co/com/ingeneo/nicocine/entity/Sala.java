b65e2373df8bc79a217009e7de0840f2c74c41a3
nicobenavides <nicolas__2@hotmail.com>
2021-07-20 23:00:22 -0500

    Update Sala Relation

--- nicocine/src/main/java/co/com/ingeneo/nicocine/entity/Sala.java
+++ nicocine/src/main/java/co/com/ingeneo/nicocine/entity/Sala.java
@@ -2,17 +2,7 @@ package co.com.ingeneo.nicocine.entity;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,9 +28,9 @@ public class Sala {
 	private String nombre;
 	
 	@JoinColumn(name = "formato_id")
-    @OneToOne(fetch = FetchType.LAZY)
+	@ManyToOne()
 	private Formato formato;
-	
+
 	/*@OneToMany(mappedBy = "sala", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Fila> filas;*/
 	
